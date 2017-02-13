8ddce2cdda292f8895150c5c3359850db42fdfd0
Ingo Hotischeck <i.hotischeck@iks-gmbh>
2017-02-13 11:47:34 +0100

    added employee form and service to create new employees with this form

--- src/app/services/mitarbeiter.service.ts
+++ src/app/services/mitarbeiter.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions } from '@angular/http';
+import { Mitarbeiter } from './../models/mitarbeiter';
+
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -19,6 +21,12 @@ export class MitarbeiterService {
   public getAlleMitarbeiter() {
     return this.http
       .get(this.backendUrl + "/mitarbeiter.json", this.requestOptions)
+      .map(res => res.json());
+  }
+
+  public erstelleMitarbeiter(mitarbeiter: Mitarbeiter) {
+    return this.http
+      .post(this.backendUrl + "/mitarbeiter.json", JSON.stringify(mitarbeiter), this.requestOptions)
       .map(res => res.json());
   }
 
