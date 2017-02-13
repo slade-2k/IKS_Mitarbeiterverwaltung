2804bad6a36c436fd2e5262c6c085e418706aa9f
Ingo Hotischeck <i.hotischeck@iks-gmbh>
2017-02-13 11:32:38 +0100

    added components and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MitarbeiterListeComponent } from './components/mitarbeiter-liste/mitarbeiter-liste.component';
+import { MitarbeiterFormularComponent } from './components/mitarbeiter-formular/mitarbeiter-formular.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+const appRoutes: Routes = [
+  { path: '', component: MitarbeiterListeComponent },
+  { path: 'anlegen', component: MitarbeiterFormularComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MitarbeiterListeComponent,
+    MitarbeiterFormularComponent,
+    NavbarComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule
---
+++ src/app/components/mitarbeiter-liste/mitarbeiter-liste.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mitarbeiter-liste',
+  templateUrl: './mitarbeiter-liste.component.html',
+  styleUrls: ['./mitarbeiter-liste.component.css']
+})
+export class MitarbeiterListeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
